14422f24c2733af12fa17470c304b75a3d8a0c23
PennLam <lemonmonlucy@163.com>
2025-01-02 17:23:15 +0800

    transfer loader to ms

--- loader.py
+++ loader.py
@@ -1,5 +1,10 @@
-import torch
-
+import mindspore
+import mindspore.dataset as ds
+import mindspore.dataset.transforms as C
+import mindspore.dataset.transforms.c_transforms as C2
+import mindspore.dataset.vision.c_transforms as CV
+import mindspore.common.dtype as mstype
+from mindspore import Tensor
 
 def load_data(path, tokenizer, device, batch_size=32, shuffle=False):
     list_tokens = []
@@ -12,16 +17,16 @@ def load_data(path, tokenizer, device, batch_size=32, shuffle=False):
                                       add_special_tokens=True,
                                       do_lower_case=False)
 
-            list_tokens.append(torch.tensor(tokens))
-            list_masks.append(torch.tensor([1] * (len(tokens))))
+            list_tokens.append(Tensor(tokens, dtype=mstype.int32))
+            list_masks.append(Tensor([1] * len(tokens), dtype=mstype.int32))
 
-    # pad the tokens, the labels and the masks
-    X = torch.nn.utils.rnn.pad_sequence(list_tokens, batch_first=True, padding_value=tokenizer.pad_token_id).to(device)
-
-    masks = torch.nn.utils.rnn.pad_sequence(list_masks, batch_first=True, padding_value=0).to(device)
+    # pad the tokens and the masks
+    pad_value = tokenizer.pad_token_id
+    X = mindspore.ops.Pad(((0, 0), (0, max(len(t) for t in list_tokens) - len(list_tokens[0]))))(Tensor(list_tokens, dtype=mstype.int32))
+    masks = mindspore.ops.Pad(((0, 0), (0, max(len(m) for m in list_masks) - len(list_masks[0]))))(Tensor(list_masks, dtype=mstype.int32))
 
     # create the loader
-    dataset = torch.utils.data.TensorDataset(X, masks)
-    loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+    dataset = ds.NumpySlicesDataset({"tokens": X, "masks": masks}, shuffle=shuffle)
+    dataset = dataset.batch(batch_size, drop_remainder=True)
 
-    return loader
+    return dataset
